a9bd35e04fdb4a514bd021d7bc0d79247ab2a05e
FedorovSemyon_2pk1 <91712962+FedorovSemyon2pk1@users.noreply.github.com>
2021-11-10 21:20:10 +0500

    Add files via upload

---
+++ fedorov_semyon2pk1/pz_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[10];
+            Console.WriteLine("Массив А: ");
+            Random rnd = new Random();
+            int i;
+            for (i = 0; i <= (9); i++) 
+            {
+                A[i]=rnd.Next(0, 10);
+                Console.Write(" " + A[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            int[] uniq = A.GroupBy(i => i).Where(i => i.Count() == 1).Select(i => i.Key).ToArray();
+            foreach (int x in uniq )
+            {
+                Console.Write(" " + x) ;
+            }
+
+        }
+    }
+}
