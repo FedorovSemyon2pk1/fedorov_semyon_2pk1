0b175513282020e46087bd62cebaf4d1fa3a5399
FedorovSemyon_2pk1 <91712962+FedorovSemyon2pk1@users.noreply.github.com>
2021-11-21 22:11:24 +0500

    Add files via upload

---
+++ pz_15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace пз_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string f1 = File.ReadAllText(@"C:\Users\ильнур\Desktop\алга\f1.txt", Encoding.UTF8);
+            string f2 = File.ReadAllText(@"C:\Users\ильнур\Desktop\алга\f2.txt", Encoding.UTF8);
+            File.WriteAllText(@"C:\Users\ильнур\Desktop\алга\h.txt", f1, Encoding.UTF8);
+            File.WriteAllText(@"C:\Users\ильнур\Desktop\алга\f2.txt",
+            File.ReadAllText(@"C:\Users\ильнур\Desktop\алга\h.txt", Encoding.UTF8),
+            Encoding.UTF8);
+            File.WriteAllText(@"C:\Users\ильнур\Desktop\алга\f1.txt", f2, Encoding.UTF8);
+
+        }
+
+
+    }
+}
