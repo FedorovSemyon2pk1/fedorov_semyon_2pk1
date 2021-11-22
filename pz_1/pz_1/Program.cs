a2866af4aff79aff7dd6f178e3dffdef7aa4b907
FedorovSemyon_2pk1 <91712962+FedorovSemyon2pk1@users.noreply.github.com>
2021-11-22 22:04:55 +0500

    Add files via upload

---
+++ pz_1/pz_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pz_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите текст:");
+            Console.ReadLine();
+            Console.WriteLine("Здорово!Напишите как у вас дела.");
+            string a = Console.ReadLine();
+            Console.WriteLine("Спасибо за ответ,хорошего вам дня и удачного настроения,кстати,рекомендую выпить чашечку чая и посмотреть в окно,сегодня там очень красиво!");
+        }
+    }
+}
