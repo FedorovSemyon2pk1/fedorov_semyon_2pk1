9204192014647b3396ce7c601ba08ea07d133c63
FedorovSemyon_2pk1 <91712962+FedorovSemyon2pk1@users.noreply.github.com>
2021-11-22 20:36:47 +0500

    Add files via upload

---
+++ pz_11/pz_11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace pz_11
+{
+    public class Example
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string output = Regex.Replace(input,
+            @"(?:\b|(?<=([A-Za-z])))([A-Z][a-z]*)",
+            m => string.Format(@"{0}{1}",
+                   (m.Groups[1].Value.Length > 0) ? "_" : "", m.Groups[2].Value.ToLower()));
+            Console.WriteLine(output);
+        }
+    }
+}
